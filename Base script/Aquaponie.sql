66a6884bcf37181a191b7f6934dbce2cabf53960
Mi-aro-101 <119170091+Mi-aro-101@users.noreply.github.com>
2023-06-22 14:42:18 -0400

    Add files via upload

--- Base script/Aquaponie.sql
+++ Base script/Aquaponie.sql
@@ -12,17 +12,25 @@ create sequence s_report_pond start with 1 increment by 1;
 create sequence s_report_field start with 1 increment by 1;
 create sequence s_sale_fish start with 1 increment by 1;
 create sequence s_sale_plantation start with 1 increment by 1;
+create sequence s_price_fish start with 1 increment by 1;
+create sequence s_price_plantation start with 1 increment by 1;
 
 create table type_fish(
     id_type_fish varchar(8) primary key,
     name_type_fish varchar(40) not null,
     maturity_period decimal(3, 1) not null, --en mois
-    maturity_size int not null -- en cm
+    maturity_size int not null, -- en cm
+    weight_max_little double precision not null, -- en gramme // Taille maximale pour la categorie petit
+    weight_max_average double precision not null,
+    size_max_little double precision not null, --en cm // Taille maximale pour la categorie moyenne
+    size_max_average double precision not null
 );
 
 create table type_plantation(
     id_type_plantation varchar(8) primary key,
-    name_type_plantation varchar(40) not null
+    name_type_plantation varchar(40) not null,
+    weight_max_baby double precision not null, -- en gram 
+    weight_max_semi_mature double precision not null
 );
 
 create table pond(
@@ -38,15 +46,16 @@ create table pond_details(
 );
 
 create table field(
-    id_field varchar(8) primary key,
-    max_plant int not null
+    id_field varchar(8) primary key
 );
 
 create table field_plantation(
     id_field_plantation int primary key,
     id_field varchar(8) references field(id_field),
     id_type_plantation varchar(8) references type_plantation(id_type_plantation),
-    plant_number int not null
+    density int, --nombre de plante ou fruit par metre carree en moyenne
+    surface_covered decimal(10,2), --surface totale couverte de plante ou fruit
+    plant_weight decimal(10,2), --en gramme : masse d'une unite plante ou fruit
     insertion_date date not null
 );
 
@@ -64,28 +73,42 @@ create table report_pond(
     id_fish_pond int references fish_pond(id_fish_pond),
     report_date_pond date not null,
     alive_fish_number int,
-    dead_fish_number int
+    dead_fish_number int,
+    category int -- Gros:21, moyen:11 ou petit:1 ilay taille
 );
 
 create table report_field(
     id_report_field int primary key,
     id_field_plantation int references field_plantation(id_field_plantation),
     report_date_field date not null,
-    one_plant_weight decimal(10,2) not null, --en gramme : masse d'un tige
-    plant_per_squaremeter decimal(10,2) not null, -- Noter seulement le nombre de plante par metre carre
+    plant_weight decimal(10,2) not null, --en gramme : masse umitaire d'une plante ouu fruit
+    density decimal(10,2) not null, -- Noter seulement le nombre de plante par metre carre
     surface_covered decimal(10,2) not null --A calculer grace au deux donnees precedent
 );
 
 create table sale_fish(
     id_sale_fish int primary key,
     id_fish_pond int references fish_pond(id_fish_pond),
-    quantity_sold int not null, -- nombre de tige vendue
+    quantity_sold int not null, -- Quantite de fish vendue en nombre satria tsy manapaka tsonjo
     sale_date date not null
 );
 
 create table sale_plantation(
     id_sale_plantation int primary key,
     id_field_plantation int references field_plantation(id_field_plantation),
-    quantity_sold int not null, --nombre de tige vendue
+    quantity_sold int not null, --nombre de plant (ou fruit) vendue
     sale_date date not null
+);
+
+create table price_fish( -- Le prix untaire designe pour une VENTE de fish
+    id_price_fish varchar(8) primary key,
+    id_type_fish varchar(8) references type_fish(id_type_fish),
+    unit_fish_price double precision not null,
+    price_category int not null --gros:21 moyen:11 ou petit:1 
+);
+
+create table price_plantation( -- Le prix untaire designe pour une VENTE de plantation
+    id_price_plantation varchar(8) primary key,
+    id_type_plantation varchar(8) references type_plantation(id_type_plantation),
+    unit_plant_price double precision not null --Prix par gramme
 );
--- Base script/Data.sql
+++ Base script/Data.sql
@@ -1,8 +1,8 @@
-insert into type_fish (id_type_fish, name_type_fish, maturity_period, maturity_size) values 
-    ('FISH0001', 'Tilapia', 24, 15); --valeur min : 24 mois -> 15 cm
+insert into type_fish (id_type_fish, name_type_fish, maturity_period, maturity_size, weight_max_little, weight_max_average, size_max_little, size_max_average) values 
+    ('FISH0001', 'Tilapia', 24, 15, 250, 500, 5, 10); --valeur min : 24 mois -> 15 cm
 
-insert into type_plantation (id_type_plantation, name_type_plantation) values
-    ('PLNT0001', 'Cresson');
+insert into type_plantation (id_type_plantation, name_type_plantation, weight_max_baby, weight_max_semi_mature) values
+    ('PLNT0001', 'Cresson', 5, 20);
 
 insert into pond(id_pond, capacity) values
     ('POND0001', 3785),
@@ -12,30 +12,38 @@ insert into pond_details (id_pond_details, id_pond, id_type_fish, max_quantity) 
     (nextval('s_pond_details'), 'POND0001', 'FISH0001', 50),
     (nextval('s_pond_details'), 'POND0002', 'FISH0001', 50);
 
-insert into field (id_field, max_surface) values
-    ('FILD0001', 10.15),
-    ('FILD0002', 10.00); -- Ie 10 gramma mihitsy ireny @ voalohany 
+insert into field(id_field) values 
+    ('FILD0001'),
+    ('FILD0002');
 
-insert into field_plantation (id_field_plantation, id_field, id_type_plantation, plant_number, insertion_date) values
-    (nextval('s_field_plantation'), 'FILD0001', 'PLNT0001', 69 '2023-05-15'),
-    (nextval('s_field_plantation'), 'FILD0002', 'PLNT0001', 95 '2023-05-1');
+insert into field_plantation (id_field_plantation, id_field, id_type_plantation, density, surface_covered, plant_weight,insertion_date) values
+    (nextval('s_field_plantation'), 'FILD0001', 'PLNT0001', 1, 0.01, 1, '06-20-2023'),
+    (nextval('s_field_plantation'), 'FILD0002', 'PLNT0001', 1, 0.01, 1, '06-20-2023');
 
-insert into fish_pond(id_fish_pond, id_type_fish, id_pond, fish_gender, quantity, insertion_date) values
-    (nextval('s_fish_pond'), 'FISH0001', 'POND0001', true, 44, '2023-05-15'),
-    (nextval('s_fish_pond'), 'FISH0001', 'POND0002', false, 50, '2023-05-15');
+insert into fish_pond (id_fish_pond, id_type_fish, id_pond, fish_gender, quantity, insertion_date) values
+    (nextval('s_fish_pond'), 'FISH0001', 'POND0001', true, 50, '06-20-2023'),
+    (nextval('s_fish_pond'), 'FISH0001', 'POND0002', false, 50, '06-20-2023');
 
-insert into report_pond(id_report_pond, report_date_pond, id_fish_pond, alive_fish_number, dead_fish_number) values
-    (nextval('s_report_pond'), '2023-06-15', 1, 40, 4),
-    (nextval('s_report_pond'), '2023-06-15', 2, 40, 10);
+insert into report_pond (id_report_pond, id_fish_pond, report_date_pond, alive_fish_number, dead_fish_number) values
+    (nextval('s_report_pond'), 1, '07-20-2023', 44, 6),
+    (nextval('s_report_pond'), 2, '07-20-2023', 45, 5);
 
-insert into report_field(id_report_field, id_field_plantation, report_date_field, one_plant_weight, plant_per_squaremeter, surface_covered) values
-    (nextval('s_report_field'), 1, '2023-06-15', 20, 26, 10.2),
-    (nextval('s_report_field'), 2, '2023-06-15', 14, 32, 10);
+insert into report_field (id_report_field, id_field_plantation, report_date_field, plant_weight, density, surface_covered) values 
+    (nextval('s_report_field'), 1, '07-20-2023', 20, 22, 9.12),
+    (nextval('s_report_field'), 2, '07-20-2023', 15, 27, 9.5);
 
-insert into sale_fish(id_sale_fish, id_fish_pond, quantity_sold, sale_date) values
-    (nextval('s_sale_fish'), 1, 16, '2023-06-17'),
-    (nextval('s_sale_fish'), 2, 22, '2023-06-17');
+insert into sale_fish (id_sale_fish, id_fish_pond, quantity_sold, sale_date) values 
+    (nextval('s_sale_fish'), 1, 14, '07-21-2023'),
+    (nextval('s_sale_fish'), 2, 1, '07-21-2023');
 
-insert into sale_plantation(id_sale_plantation, id_field_plantation, quantity_sold, sale_date) values
-    (nextval('s_sale_plantation'), 1, 25, '2023-06-17' ),
-    (nextval('s_sale_plantation'), 2, 25, '2023-06-17' );
+insert into sale_plantation (id_sale_plantation, id_field_plantation, quantity_sold, sale_date) values
+    (nextval('s_sale_plantation'), 1, 62, '07-21-2023'), -- Nombre de plant (ou fruit) vendue ilay 62
+    (nextval('s_sale_plantation'), 2, 23, '07-21-2023');
+
+insert into price_fish (id_price_fish, id_type_fish, unit_fish_price, price_category) values
+    ('PFSH0001', 'FISH0001', 1000, 1),
+    ('PFSH0002', 'FISH0001', 2000, 11),
+    ('PFSH0003', 'FISH0001', 4000, 21);
+
+insert into price_plantation (id_price_plantation, id_type_plantation, unit_plant_price) values
+    ('PPLT0001', 'PLNT0001', 30);
