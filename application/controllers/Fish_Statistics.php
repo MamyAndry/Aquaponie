eb22ce3eb95cab8ae335dc219e0488fed34029d8
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2023-07-02 10:18:31 -0700

    review

--- application/controllers/Fish_Statistics.php
+++ application/controllers/Fish_Statistics.php
@@ -15,15 +15,10 @@ class Fish_statistics extends CI_Controller {
 	// To list all type of fishes
 	public function index(){
 
-		$sold = $this->fish_statistics->get_fish_sold();
-		$year = $this->fish_statistics->get_all_year();
-		$this->data['year'] = $year;
-		$this->data['sold'] = $sold;
+		$this->data['year'] = $this->fish_statistics->get_all_year();
+		$this->data['sold'] = $this->fish_statistics->get_fish_sold();
 		$this->data['page_title'] = "Statistics of fish sold";
 		$this->data['body'] = 'statistics/fish_sold';
-		// var_dump($year);
-		// print_r($this->data);
-		// var_dump($sold);
 		$this->load->view('template/index' , $this->data);
 
 	}
